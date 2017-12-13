767b5d5853dddf154992192d9e5edbdb9b0dbc2f
leondraceus <hathaihiepluan@gmail.com>
2017-12-13 21:16:24 +0700

    up

--- cmds/smash.js
+++ cmds/smash.js
@@ -1,7 +1,7 @@
 module.exports.run = (pw, message, args) => {
     var toMention = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
     if(!toMention) return message.channel.sendMessage(`Bạn phải mention một ai đó!`)
-    message.channel.sendMessage(message.author.nickname + ' đã đập ' + args[0] + '!');
+    message.channel.server.detailsOf(message.author).nick;
     console.log(`${message.author.username}#${message.author.discriminator} used smash!`);
 }
 
