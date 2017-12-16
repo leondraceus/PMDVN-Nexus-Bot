f25e1908035748d8d3f0694a5b8ab4f72ffd09ec
leondraceus <hathaihiepluan@gmail.com>
2017-12-16 15:04:49 +0700

    up

--- cmds/inv.js
+++ cmds/inv.js
@@ -1374,6 +1374,19 @@ Vui lòng dùng code để tra cứu theo các lệnh sau: **.inv ME514** | **.i
             
         message.channel.sendEmbed(ME868);
     }
+
+    /// Cutiefly (ME566) ///
+    if(args[0]==="ME566"| args[0]==="me566" | args[0]==="Me566" | args[0]==="Cutiefly" | args[0]==="cutiefly"){
+        var ME566 = new Discord.RichEmbed().setColor(`#3A77E6`)
+            .setTitle(`- THÔNG TIN -`)
+            .setAuthor(`Cutiefly/Bùi Nhật Huy`,`https://vignette.wikia.nocookie.net/pmdv-nexus/images/6/6a/Serperior.png/revision/latest/scale-to-width-down/100?cb=20160904162403`)
+            .setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/a/a6/Member_Logo.png/revision/latest?cb=20170613044002`)
+            .addField(`Poké:`,`10`,inline = true)
+            .addField(`Spooky Token:`,`0`,inline = true)
+            .addField(`Không có Item nào trong kho đồ...`,`\u200B`);
+            
+        message.channel.sendEmbed(ME566);
+    }
 }
 
 module.exports.help = {
