09853cdff0b3127973fff6561da1c50793acff0b
leondraceus <hathaihiepluan@gmail.com>
2017-12-17 16:31:44 +0700

    up

--- cmds/info.js
+++ cmds/info.js
@@ -1360,7 +1360,7 @@ Vui lòng dùng code để tra cứu theo các lệnh sau: **.info ME514** | **.
             .setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/a/a6/Member_Logo.png/revision/latest?cb=20170613044002`)
             .addField(`Chủng loài:`,`Serperior`)
             .addField(`Tên Facebook:`,`Nguyễn Hoài Thu`)
-            .addField(`Giới tính:`,`Nam`)
+            .addField(`Giới tính:`,`Nữ`)
             .addField(`Ngày sinh:`,`11/6`)
             .addField(`Cấp bậc:`,`Member`)
             .addField(`Guild:`,`---`)
