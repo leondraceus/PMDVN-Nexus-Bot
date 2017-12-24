dcf550d08ecd9b1e1c8a290c7e43ae57b679366c
leondraceus <hathaihiepluan@gmail.com>
2017-12-24 14:41:14 +0700

    up

---
+++ cmds/delibird.js
@@ -0,0 +1,14 @@
+module.exports.run = async (pw, message, args) => {
+    function getRandomInt(min, max) {
+       return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    var roll = Math.floor(Math.random() * 5) +1;
+    if (roll = 1, 2, 3)
+    message.reply("bạn nhận được một nụ hôn");
+    else
+    message.reply("bạn nhận được một nụ hôn nồng thắm");
+}
+
+module.exports.help = {
+    name: "delibird"
+}
