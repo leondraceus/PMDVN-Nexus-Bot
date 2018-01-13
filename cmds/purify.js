ef4ed3a14f38c8ebd6a767a614d20f593c8e5c9b
leondraceus <hathaihiepluan@gmail.com>
2018-01-13 18:33:48 +0700

    up

---
+++ cmds/purify.js
@@ -0,0 +1,23 @@
+const Discord = module.require("discord.js");
+
+module.exports.run = async (pw, message, args) => {
+    var embed = new Discord.RichEmbed()
+    .setColor(`#556a74`)
+    .setDescription(args[0] +` đã được hoá giải khỏi sự hoá đá!`);
+
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.sendMessage("```Bạn không đủ quyền hành để thực hiện việc này!```");
+
+    var toPurify = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!toPurify) return message.channel.sendMessage("```Hãy Mention một ai đó!```");
+
+    var role = message.guild.roles.find(r => r.name === "Petrified");
+
+    if(!toPurify.roles.has(role.id)) return message.channel.sendMessage("```Pokémon này hiện không bị hoá đá!```");
+
+    await toPurify.removeRole(role);
+    message.channel.sendEmbed(embed);
+}
+
+module.exports.help = {
+    name: "purify"
+}
---
+++ cmds/vomit.js
@@ -0,0 +1,23 @@
+const Discord = module.require("discord.js");
+
+module.exports.run = async (pw, message, args) => {
+    var embed = new Discord.RichEmbed()
+       .setColor(`#923200`)
+       .setDescription(args[0] +` đã được nhả ra!`);
+
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.sendMessage("```Bạn không đủ quyền hành để thực hiện việc này!```");
+
+    var toVomit = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!toVomit) return message.channel.sendMessage("```Hãy Mention một ai đó!```");
+
+    var role = message.guild.roles.find(r => r.name === "Eaten");
+
+    if(!toVomit.roles.has(role.id)) return message.channel.sendMessage("```Pokémon này hiện không ở trong bụng!```");
+
+    await toVomit.removeRole(role);
+    message.channel.sendEmbed(embed);
+}
+
+module.exports.help = {
+    name: "vomit"
+}
