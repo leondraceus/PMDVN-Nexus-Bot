b5a101f1259189bce43ee61a60b9081269a89063
leondraceus <hathaihiepluan@gmail.com>
2018-01-15 14:25:06 +0700

    up

--- cmds/info.js
+++ cmds/info.js
@@ -1544,6 +1544,23 @@ Vui lòng dùng code để tra cứu theo các lệnh sau: **.info ME401** | **.
             
         message.channel.sendEmbed(ME362);
     }
+
+    /// Glaceon (ME544) ///
+    if(args[0]==="ME544"| args[0]==="me544" | args[0]==="Me544" | args[0]==="Glaceon" | args[0]==="glaceon"){
+        var ME544 = new Discord.RichEmbed().setColor(`#3A77E6`)
+            .setTitle(`- THÔNG TIN -`)
+            .setAuthor(`Glaceon/Aoi Miyuki`,`https://vignette.wikia.nocookie.net/pmdv-nexus/images/8/8a/Glaceon1.png/revision/latest/scale-to-width-down/100?cb=20180115071344`)
+            .setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/a/a6/Member_Logo.png/revision/latest?cb=20170613044002`)
+            .addField(`Chủng loài:`,`Glaceon`)
+            .addField(`Tên Facebook:`,`Aoi Miyuki`)
+            .addField(`Giới tính:`,`Nam`)
+            .addField(`Ngày sinh:`,`17/1`)
+            .addField(`Cấp bậc:`,`Member`)
+            .addField(`Guild:`,`---`)
+            .addField(`Thành tích:`,`---`);
+            
+        message.channel.sendEmbed(ME544);
+    }
 }
 
 module.exports.help = {
--- cmds/inv.js
+++ cmds/inv.js
@@ -1700,6 +1700,18 @@ Vui lòng dùng code để tra cứu theo các lệnh sau: **.inv ME401** | **.i
             
         message.channel.sendEmbed(ME362);
     }
+
+    /// Glaceon (ME544) ///
+    if(args[0]==="ME544"| args[0]==="me544" | args[0]==="Me544" | args[0]==="Glaceon" | args[0]==="glaceon"){
+        var ME544 = new Discord.RichEmbed().setColor(`#3A77E6`)
+            .setTitle(`- KHO ĐỒ -`)
+            .setAuthor(`Glaceon/Aoi Miyuki`,`https://vignette.wikia.nocookie.net/pmdv-nexus/images/8/8a/Glaceon1.png/revision/latest/scale-to-width-down/100?cb=20180115071344`)
+            .setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/a/a6/Member_Logo.png/revision/latest?cb=20170613044002`)
+            .addField(`Poké:`,`0`,inline = true)
+            .addField(`Không có Item nào trong kho đồ...`,`\u200B`);
+            
+        message.channel.sendEmbed(ME544);
+    }
 }
 
 module.exports.help = {
