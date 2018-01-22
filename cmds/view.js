e76e1d14ebe7e7d1bf1069218a9e80bd7cce5120
leondraceus <hathaihiepluan@gmail.com>
2018-01-22 09:41:05 +0700

    up

--- cmds/view.js
+++ cmds/view.js
@@ -22,33 +22,81 @@ module.exports.run = async (pw, message, args) => {
     if(args[0]==="doll"){
         var doll = new Discord.RichEmbed()
             .setTitle(`- Collectible Dolls -`).setColor(`#7FFFD4`).setURL(`http://pmdv-nexus.wikia.com/wiki/Kecleon's_Shop`).setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/5/59/Kecleon1.png/revision/latest/scale-to-width-down/56?cb=20170620115423`)
+            .addField(`Gen 1 Starters & Electric Rodent Set`,`.view pack001`)
+            .addField(`Gen 2 Starters & Electric Rodent Set`,`.view pack002`)
+            .addField(`Gen 3 Starters & Electric Rodent Set`,`.view pack003`)
+            .addField(`Gen 4 Starters & Electric Rodent Set`,`.view pack004`)
+            .addField(`Gen 5 Starters & Electric Rodent Set`,`.view pack005`)
+            .addField(`Gen 6 Starters & Electric Rodent Set`,`.view pack006`);
+
+        message.channel.sendEmbed(doll);
+    }
+    
+    if(args[0]==="pack001"){
+        var pack001 = new Discord.RichEmbed()
+            .setTitle(`- Collectible Dolls -`).setColor(`#7FFFD4`).setURL(`http://pmdv-nexus.wikia.com/wiki/Kecleon's_Shop`).setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/5/59/Kecleon1.png/revision/latest/scale-to-width-down/56?cb=20170620115423`)
             .addField(`Bulbasaur Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Bulbasaur.`)
             .addField(`Charmander Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Charmander.`)
             .addField(`Squirtle Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Squirtle.`)
-            .addField(`Pikachu Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Pikachu.`)
+            .addField(`Pikachu Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Pikachu.`);
+
+        message.channel.sendEmbed(pack001);
+    }
+
+    if(args[0]==="pack002"){
+        var pack002 = new Discord.RichEmbed()
+            .setTitle(`- Collectible Dolls -`).setColor(`#7FFFD4`).setURL(`http://pmdv-nexus.wikia.com/wiki/Kecleon's_Shop`).setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/5/59/Kecleon1.png/revision/latest/scale-to-width-down/56?cb=20170620115423`)
             .addField(`Chikorita Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Chikorita.`)
             .addField(`Cyndaquil Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Cyndaquil.`)
             .addField(`Totodile Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Totodile.`)
-            .addField(`Pichu Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Pichu.`)
+            .addField(`Pichu Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Pichu.`);
+
+        message.channel.sendEmbed(pack002);
+    }
+
+    if(args[0]==="pack003"){
+        var pack003 = new Discord.RichEmbed()
+            .setTitle(`- Collectible Dolls -`).setColor(`#7FFFD4`).setURL(`http://pmdv-nexus.wikia.com/wiki/Kecleon's_Shop`).setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/5/59/Kecleon1.png/revision/latest/scale-to-width-down/56?cb=20170620115423`)
             .addField(`Treecko Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Treecko.`)
             .addField(`Torchic Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Torchic.`)
             .addField(`Mudkip Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Mudkip.`)
             .addField(`Plusle Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Plusle.`)
-            .addField(`Minun Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Minun.`)
+            .addField(`Minun Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Minun.`);
+
+        message.channel.sendEmbed(pack003);
+    } 
+
+    if(args[0]==="pack004"){
+        var pack004 = new Discord.RichEmbed()
+            .setTitle(`- Collectible Dolls -`).setColor(`#7FFFD4`).setURL(`http://pmdv-nexus.wikia.com/wiki/Kecleon's_Shop`).setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/5/59/Kecleon1.png/revision/latest/scale-to-width-down/56?cb=20170620115423`)
             .addField(`Turtwig Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Turtwig.`)
             .addField(`Chimchar Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Chimchar.`)
             .addField(`Piplup Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Piplup.`)
-            .addField(`Pachirisu Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Pachirisu.`)
+            .addField(`Pachirisu Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Pachirisu.`);
+
+        message.channel.sendEmbed(pack004);
+    } 
+
+    if(args[0]==="pack005"){
+        var pack005 = new Discord.RichEmbed()
+            .setTitle(`- Collectible Dolls -`).setColor(`#7FFFD4`).setURL(`http://pmdv-nexus.wikia.com/wiki/Kecleon's_Shop`).setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/5/59/Kecleon1.png/revision/latest/scale-to-width-down/56?cb=20170620115423`)
             .addField(`Snivy Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Snivy.`)
             .addField(`Tepig Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Tepig.`)
             .addField(`Oshawott Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Oshawott.`)
-            .addField(`Emolga Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Emolga.`)
+            .addField(`Emolga Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Emolga.`);
+
+        message.channel.sendEmbed(pack005);
+    } 
+
+    if(args[0]==="pack006"){
+        var pack006 = new Discord.RichEmbed()
+            .setTitle(`- Collectible Dolls -`).setColor(`#7FFFD4`).setURL(`http://pmdv-nexus.wikia.com/wiki/Kecleon's_Shop`).setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/5/59/Kecleon1.png/revision/latest/scale-to-width-down/56?cb=20170620115423`)
             .addField(`Chespin Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Chespin.`)
             .addField(`Fennekin Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Fennekin.`)
             .addField(`Froakie Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Froakie.`)
-            .addField(`Dedenne Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Dedenne.`)
+            .addField(`Dedenne Doll (350 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Dedenne.`);
 
-        message.channel.sendEmbed(doll);
+        message.channel.sendEmbed(pack006);
     }
 
     // Collectible Items //
