dc6a93b19b674c4ca164fac9592b053af5de941b
Keiiryto <shayrlslarkd56@gmail.com>
2018-01-23 21:53:23 +0700

    up

--- cmds/hug.js
+++ cmds/hug.js
@@ -1,7 +1,7 @@
 module.exports.run = (pw, message, args) => {
     var toMention = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
     if(!toMention) return message.channel.sendMessage(`Bạn phải mention một ai đó!`)
-    message.channel.sendMessage(message.author.username + ' đã ôm ' + args[0] + ' thật chặt! (づ｡◕‿‿◕｡)づ ');
+    message.channel.sendMessage(message.author.username + ' đã ôm ' + args[0] + ' thật chặt! (づ｡◕‿‿◕｡)づ https://media.giphy.com/media/8tpiC1JAYVMFq/giphy.gif');
     console.log(`${message.author.username}#${message.author.discriminator} used hug!`);
 }
 
--- cmds/kiss.js
+++ cmds/kiss.js
@@ -1,7 +1,7 @@
 ﻿module.exports.run = (pw, message, args) => {
     var toMention = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
     if(!toMention) return message.channel.sendMessage(`Bạn phải mention một ai đó!`)
-    message.channel.sendMessage(message.author.username + ' đã hôn ' + args[0] + ':heart: !');
+    message.channel.sendMessage(message.author.username + ' đã hôn ' + args[0] + ':heart: ! http://i0.kym-cdn.com/photos/images/original/000/699/198/573.gif');
     console.log(`${message.author.username}#${message.author.discriminator} used kiss!`);
 }
 
