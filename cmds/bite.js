ee5a9b9b4ee1a20f972699d65324e9c2d118e12b
leondraceus <hathaihiepluan@gmail.com>
2018-02-03 20:21:10 +0700

    up

--- cmds/bite.js
+++ cmds/bite.js
@@ -2,7 +2,6 @@ module.exports.run = (pw, message, args) => {
     var toMention = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
     if(!toMention) return message.channel.sendMessage(`Bạn phải mention một ai đó!`)
     message.channel.sendMessage(message.author.username + ' đã cắn ' + args[0] + ' :heart: https://31.media.tumblr.com/9698474b91588cdd9ded9c3d2eb85515/tumblr_inline_n34t5eNZ6t1r03dsk.gif ');
-    console.log(`${message.author.username}#${message.author.discriminator} used bite!`);
 }
 
 module.exports.help = {
--- cmds/lick.js
+++ cmds/lick.js
@@ -2,7 +2,7 @@
     var toMention = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
     if(!toMention) return message.channel.sendMessage(`Bạn phải mention một ai đó!`)
     message.channel.sendMessage(message.author.username + ' đã liếm ' + args[0] + ' (๑>◡<๑) ! https://pa1.narvii.com/6415/99af997dbefc5885ebe12e497c5a4cfdc4b03843_hq.gif ');
-console.log(`${message.author.username}#${message.author.discriminator} used lick!`);
+console.log(`${message.author.username}#${message.author.discriminator} used lick`);
 }
 
 module.exports.help = {
