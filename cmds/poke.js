6b137877b575c570c9510d7aace6b8a1966f60b8
leondraceus <hathaihiepluan@gmail.com>
2018-02-03 20:38:41 +0700

    up

--- cmds/poke.js
+++ cmds/poke.js
@@ -2,7 +2,6 @@ module.exports.run = (pw, message, args) => {
     var toMention = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
     if(!toMention) return message.channel.sendMessage(`Bạn phải mention một ai đó!`)
     message.channel.sendMessage(message.author.username + ' đã chọc ' + args[0] + ' 👉 ');
-    console.log(`${message.author.username}#${message.author.discriminator} used poke!`);
 }
 
 module.exports.help = {
