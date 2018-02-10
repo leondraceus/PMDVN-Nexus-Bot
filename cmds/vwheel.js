5ef3436a50438d0a1d0b0da01cce1f14e79a0eb2
leondraceus <hathaihiepluan@gmail.com>
2018-02-10 21:00:06 +0700

    up

--- cmds/vwheel.js
+++ cmds/vwheel.js
@@ -6,19 +6,19 @@ module.exports.run = async (pw, message, args) => {
 
     message.channel.send("<@&374117121745223680>");
     if (roll == 1 || roll == 2 || roll == 3 || roll == 4 || roll == 5 || roll == 6 || roll == 7 || roll == 8 || roll == 9 || roll == 10 || roll == 11 || roll == 12 || roll == 13 || roll == 14 || roll == 15 || roll == 16 || roll == 17 || roll == 18 || roll == 19 || roll == 20)
-    message.reply("bạn đã quay trúng 20 <:poke:379535433467428867>, chúc mừng! <:laugh1:374121408759988224>");
+    message.reply("bạn đã quay trúng 15 <:poke:379535433467428867>, chúc mừng! <:laugh1:374121408759988224>");
 
     else if (roll == 21 || roll == 22 || roll == 23 || roll == 24 || roll == 25 || roll == 26 || roll == 27 || roll == 28 || roll == 29 || roll == 30 || roll == 31 || roll == 32 || roll == 33 || roll == 34 || roll == 35)
-    message.reply("bạn đã quay trúng 100 <:poke:379535433467428867>, chúc mừng! <:laugh1:374121408759988224>");
+    message.reply("bạn đã quay trúng 75 <:poke:379535433467428867>, chúc mừng! <:laugh1:374121408759988224>");
 
     else if (roll == 36 || roll == 37 || roll == 38 || roll == 39 || roll == 40 || roll == 41 || roll == 42 || roll == 43 || roll == 44 || roll == 45)
-    message.reply("bạn đã quay trúng 200 <:poke:379535433467428867>, chúc mừng! <:laugh1:374121408759988224>");
+    message.reply("bạn đã quay trúng 150 <:poke:379535433467428867>, chúc mừng! <:laugh1:374121408759988224>");
 
     else if (roll == 46 || roll == 47 || roll == 48 || roll == 49 || roll == 50)
-    message.reply("bạn đã quay trúng 400 <:poke:379535433467428867>, chúc mừng! <:laugh1:374121408759988224>");
+    message.reply("bạn đã quay trúng 300 <:poke:379535433467428867>, chúc mừng! <:laugh1:374121408759988224>");
 
     else if (roll == 51 || roll == 52)
-    message.reply("bạn đã quay trúng 1000 <:poke:379535433467428867>, chúc mừng! <:laugh1:374121408759988224>");
+    message.reply("bạn đã quay trúng 750 <:poke:379535433467428867>, chúc mừng! <:laugh1:374121408759988224>");
 
     else if (roll == 53 || roll == 54 || roll == 55 || roll == 56 || roll == 57 || roll == 58 || roll == 59 || roll == 60 || roll == 61 || roll == 62 || roll == 63 || roll == 64 || roll == 65 || roll == 66 || roll == 67)
     message.reply("bạn đã quay trúng Salt/Corsola Twig (Tùy chọn), chúc mừng! <:laugh1:374121408759988224>");
