e1a03a9db1c271c36a7f86aa6f914aeb3c1ca822
leondraceus <hathaihiepluan@gmail.com>
2018-02-10 08:01:49 +0700

    up

--- cmds/inv.js
+++ cmds/inv.js
@@ -1628,14 +1628,14 @@ Vui lòng dùng code để tra cứu theo các lệnh sau: **.inv ME514** | **.i
             .setTitle(`- KHO ĐỒ -`)
             .setAuthor(`Serperior/Nguyễn Hoài Thu`,`https://vignette.wikia.nocookie.net/pmdv-nexus/images/6/6a/Serperior.png/revision/latest/scale-to-width-down/100?cb=20160904162403`)
             .setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/a/a6/Member_Logo.png/revision/latest?cb=20170613044002`)
-            .addField(`Poké:`,`540`,inline = true)
+            .addField(`Poké:`,`390`,inline = true)
             .addField(`Spooky Token:`,`1`,inline = true)
             .addField(`Snowflake:`,`1`,inline = true)
             .addField(`Yveltal Doll`,`Một con búp bê hình Pokémon Yveltal.`)
             .addField(`Snover Doll`,`Một con búp bê hình Pokémon Snover.`)
             .addField(`Delibird Doll`,`Một con búp bê hình Pokémon Delibird.`)
             .addField(`Snivy Doll`,`Một con búp bê hình Pokémon Snivy.`)
-            .addField(`Furfrou Doll`,`Một con búp bê hình Pokémon Furfrou.`);
+            .addField(`Furfrou Doll (La Reine)`,`Một con búp bê hình Pokémon Furfrou (La Reine Trim).`);
             
         message.channel.sendEmbed(ME868);
     }
