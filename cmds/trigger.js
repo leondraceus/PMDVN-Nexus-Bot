9efae0ec32f9b57bcf206f8e3f987aeb267c1ce6
leondraceus <hathaihiepluan@gmail.com>
2018-02-18 20:11:53 +0700

    up

--- cmds/trigger.js
+++ cmds/trigger.js
@@ -1,5 +1,5 @@
 ﻿module.exports.run = async (pw, message, args) => {
-    message.channel.sendMessage(message.author.username + " is triggering (┛ಠ□ಠ)┛︵┻━┻ https://media1.tenor.com/images/bc1050171b5f12054d618e57e312d6b2/tenor.gif");
+    message.channel.sendMessage(message.author.username + " đang trigger (┛ಠ□ಠ)┛︵┻━┻ https://media1.tenor.com/images/bc1050171b5f12054d618e57e312d6b2/tenor.gif");
 }
 
 module.exports.help = {
