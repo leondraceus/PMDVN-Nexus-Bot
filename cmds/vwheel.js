ebeea7de2f68636753a2aa143aac5b3879f31fbe
leondraceus <hathaihiepluan@gmail.com>
2018-04-01 09:58:49 +0700

    up

--- cmds/vwheel.js
+++ cmds/vwheel.js
@@ -8,28 +8,28 @@ module.exports.run = async (pw, message, args) => {
     if (roll < 11)
     message.reply("chúc mừng ngày quốc tế xạo lone :heart:");
 
-    else if (10 < roll < 21)
+    else if (roll < 21)
     message.reply("bạn đoé được cái gì hết, tiếc quá ~ :heart:");
 
-    else if (20 < roll < 31)
+    else if (roll < 31)
     message.reply("xin chúc mừng, bạn đã được 1 triệu :poke:.... có cl í :fakesmile:");
 
-    else if (30< roll < 41)
+    else if (roll < 41)
     message.reply("tặng bạn 1 random fact: Con De kém sang");
 
-    else if (40 < roll < 51)
+    else if (roll < 51)
     message.reply("bạn không được làm thế. Bạn làm thế là bạn xạo lone.");
 
-    else if (50 < roll < 61)
+    else if (roll < 61)
     message.reply("tặng bạn 1 random fact: Admin Zekrom Đen Hôi đẹp trai 6 múi khoai to soái ca");
 
-    else if (60 < roll < 71)
+    else if (roll < 71)
     message.reply("let the trigger begin! (┛ಠ□ಠ)┛︵┻━┻");
 
-    else if (70 < roll < 81)
+    else if (roll < 81)
     message.reply("bạn rất là đẹp.... có cl nha :heart:");
 
-    else if (80 < roll < 91)
+    else if (roll < 91)
     message.reply("chào mừng đến với thế giới xạo lone.s");
 
     else
