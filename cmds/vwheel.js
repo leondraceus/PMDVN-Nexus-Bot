cf980f13304ad35608a6c2a29d9b42b209e813f7
leondraceus <hathaihiepluan@gmail.com>
2018-04-01 09:46:25 +0700

    up

--- cmds/vwheel.js
+++ cmds/vwheel.js
@@ -2,37 +2,46 @@ module.exports.run = async (pw, message, args) => {
     function getRandomInt(min, max) {
        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    var roll = Math.floor(Math.random() * 100) +1;
+    var roll = Math.floor(Math.random() * 10) +1;
+    var roll1 = Math.floor(Math.random() * 10) +11;
+    var roll2 = Math.floor(Math.random() * 10) +21;
+    var roll3 = Math.floor(Math.random() * 10) +31;
+    var roll4 = Math.floor(Math.random() * 10) +41;
+    var roll5 = Math.floor(Math.random() * 10) +51;
+    var roll6 = Math.floor(Math.random() * 10) +61;
+    var roll7 = Math.floor(Math.random() * 10) +71;
+    var roll8 = Math.floor(Math.random() * 10) +81;
+    var roll9 = Math.floor(Math.random() * 10) +91;
 
     message.channel.send("<@&374117121745223680>");
-    if (1 <= roll <= 10)
+    if (roll)
     message.reply("chúc mừng ngày quốc tế xạo lone :heart:");
 
-    else if (11 <= roll <= 20)
+    else if (roll1)
     message.reply("bạn đoé được cái gì hết, tiếc quá ~ :heart:");
 
-    else if (21 <= roll <= 30)
+    else if (roll2)
     message.reply("xin chúc mừng, bạn đã được 1 triệu :poke:.... có cl í :fakesmile:");
 
-    else if (31 <= roll <= 40)
+    else if (roll3)
     message.reply("tặng bạn 1 random fact: Con De kém sang");
 
-    else if (41 <= roll <= 50)
+    else if (roll4)
     message.reply("bạn không được làm thế. Bạn làm thế là bạn xạo lone.");
 
-    else if (51 <= roll <= 60)
+    else if (roll5)
     message.reply("tặng bạn 1 random fact: Admin Zekrom Đen Hôi đẹp trai 6 múi khoai to soái ca");
 
-    else if (61 <= roll <= 70)
+    else if (roll6)
     message.reply("let the trigger begin! (┛ಠ□ಠ)┛︵┻━┻");
 
-    else if (71 <= roll <= 80)
+    else if (roll7)
     message.reply("bạn rất là đẹp.... có cl nha :heart:");
 
-    else if (81 <= roll <= 90)
+    else if (roll8)
     message.reply("chào mừng đến với thế giới xạo lone");
 
-    else if (91 <= roll <= 100)
+    else if (roll9)
     message.reply("mình yêu bạn");
 }
 
