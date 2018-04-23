00c1d219c60fe8b88f7387128af6c2f4a83df7d3
leondraceus <hathaihiepluan@gmail.com>
2018-04-23 14:02:01 +0700

    up

--- cmds/info.js
+++ cmds/info.js
@@ -1149,8 +1149,8 @@ Vui lòng dùng code để tra cứu theo các lệnh sau: **.info ME099** | **.
             .setTitle(`- THÔNG TIN -`).setImage(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/c/c3/Ribbon_Banner.png/revision/latest?cb=20180215033233`)
             .setAuthor (`Skitty/Đoàn Thị Thanh`,`https://vignette.wikia.nocookie.net/pmdv-nexus/images/f/f0/Skitty.png/revision/latest/scale-to-width-down/100?cb=20180423051337`)
             .setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/a/a6/Member_Logo.png/revision/latest?cb=20170613044002`)
-            .addField(`Chủng loài:`,`Purrloin`)
-            .addField(`Tên Facebook:`,`Đoàn Anh Đức`)
+            .addField(`Chủng loài:`,`Skitty`)
+            .addField(`Tên Facebook:`,`Đoàn Thị Thanh`)
             .addField(`Giới tính:`,`Nữ`)
             .addField(`Ngày sinh:`,`16/12`)
             .addField(`Cấp bậc:`,`Member`)
@@ -1911,7 +1911,7 @@ Vui lòng dùng code để tra cứu theo các lệnh sau: **.info ME983** | **.
     }
 
     /// Black Kyurem (ME303) ///
-    if(args[0]==="ME303"| args[0]==="me303" | args[0]==="Me303"){
+    if(args[0]==="ME303"| args[0]==="me303" | args[0]==="Me303" | args[0]==="Blackkyurem" | args[0]==="blackkyurem" | args[0]==="BlackKyurem"){
         var ME303 = new Discord.RichEmbed().setColor(`#3A77E6`)
             .setTitle(`- THÔNG TIN -`).setImage(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/d/d0/Kyurem_Black_XY.gif`)
             .setAuthor(`Black Kyurem/Bảo Nguyễn`,`https://vignette.wikia.nocookie.net/pmdv-nexus/images/a/a4/Black_Kyurem.png/revision/latest?cb=20180422102023`)
