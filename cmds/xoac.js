4430a2f56974b60b1c154c18c598bf0fabd151bf
Keiiryto <shayrlslarkd56@gmail.com>
2018-05-08 12:35:38 +0700

    up

--- cmds/xoac.js
+++ cmds/xoac.js
@@ -1,9 +1,9 @@
 ﻿module.exports.run = (pw, message, args) => {
     var toMention = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
     if(!toMention) return message.channel.sendMessage(`Bạn phải gạ xoạc một ai đó!`)
-    message.channel.sendMessage(message.author.username + ' đã gạ xoạc với ' + args[0] + ':heart: ! https://vignette.wikia.nocookie.net/degrassi/images/8/89/Funny-pokemon-gif-piplup-steal.gif/revision/latest?cb=20150526155747');
+    message.channel.sendMessage(message.author.username + ' đã gạ xoạc với ' + args[0] + ':heart: ! http://2.bp.blogspot.com/-DMmg9d0YRVg/UGLoofj9UUI/AAAAAAAACas/kTPkU7EJX_I/s1600/bellsprout+wiggle.gif');
 }
 
 module.exports.help = {
-    name: "xoac"
+    name: "xoạc"
 }
