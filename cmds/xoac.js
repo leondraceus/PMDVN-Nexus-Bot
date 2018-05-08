df151d82bc66f514291b61796f1f4a0a03493191
Keiiryto <shayrlslarkd56@gmail.com>
2018-05-08 13:05:18 +0700

    up

--- cmds/xoac.js
+++
@@ -1,9 +0,0 @@
-﻿////module.exports.run = (pw, message, args) => {
-////    var toMention = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-////    if(!toMention) return message.channel.sendMessage(`Bạn phải gạ xoạc một ai đó!`)
-////    message.channel.sendMessage(message.author.username + ' đã gạ xoạc với ' + args[0] + ':heart: !);
-////}
-
-////module.exports.help = {
-////    name: "xoac"
-////}
