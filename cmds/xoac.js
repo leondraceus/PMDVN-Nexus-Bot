b5a62a4d319fec6e2daf69655baeb94a1f1d0a1c
Keiiryto <shayrlslarkd56@gmail.com>
2018-05-08 13:03:55 +0700

    up

--- cmds/xoac.js
+++ cmds/xoac.js
@@ -1,9 +1,9 @@
-﻿//module.exports.run = (pw, message, args) => {
-//    var toMention = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-//    if(!toMention) return message.channel.sendMessage(`Bạn phải gạ xoạc một ai đó!`)
-//    message.channel.sendMessage(message.author.username + ' đã gạ xoạc với ' + args[0] + ':heart: ! http://2.bp.blogspot.com/-DMmg9d0YRVg/UGLoofj9UUI/AAAAAAAACas/kTPkU7EJX_I/s1600/bellsprout+wiggle.gif');
-//}
+﻿////module.exports.run = (pw, message, args) => {
+////    var toMention = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+////    if(!toMention) return message.channel.sendMessage(`Bạn phải gạ xoạc một ai đó!`)
+////    message.channel.sendMessage(message.author.username + ' đã gạ xoạc với ' + args[0] + ':heart: !);
+////}
 
-//module.exports.help = {
-//    name: "xoac"
-//}
+////module.exports.help = {
+////    name: "xoac"
+////}
