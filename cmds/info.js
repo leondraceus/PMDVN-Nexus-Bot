8df99a781aaa3caebc25da7e1729ee2676bb6520
Keiiryto <shayrlslarkd56@gmail.com>
2018-05-10 23:22:12 +0700

    up

--- cmds/info.js
+++ cmds/info.js
@@ -867,9 +867,11 @@ Vui lòng dùng code để tra cứu theo các lệnh sau: **.info ME459** | **.
             .addField(`Tổ chức Member Event:`,`Pokémon Kicking Contest (24/12/2017 -> 1/1/2018)`)
             .addField(`Tổ chức Member Event:`,`Time to Explode (1/1/2018 -> 6/1/2018)`)
             .addField(`Tổ chức Member Event:`,`Fast and Furious (14/1/2018)`)
-            .addField(`Heart Festival:`,`Giải ba (Meowth Theater)`)
+            .addField(`Giải ba:`,`Heart Festival (Meowth Theater)`)
             .addField(`Tổ chức Member Event:`,`Berry Collector (25/2/2018 -> 25/2/2018)`)
-            .addField(`Tổ chức Member Event:`,`Couple Race (18/3/2018)`);
+            .addField(`Tổ chức Member Event:`,`Couple Race (18/3/2018)`)
+            .addField(`Giải nhì:`,`The Sky Challenges (Zapdos Challenge)`)
+            .addField(`Giải ba:`,`The Sky Challenges (Articuno Challenge)`);
 
         message.channel.sendEmbed(ME072);
         message.channel.sendEmbed(ACME072);
@@ -1965,10 +1967,15 @@ Vui lòng dùng code để tra cứu theo các lệnh sau: **.info ME983** | **.
             .addField(`Ngày sinh:`,`29/2`)
             .addField(`Cấp bậc:`,`Member`)
             .addField(`Guild:`,`---`)
-            .addField(`Thành tích:`,`Tổ chức Member Event: Is it your final choice? (25/3/2018)`);
-            
-        message.channel.sendEmbed(ME394);
-    }
+             var ACME394 = new Discord.RichEmbed().setColor(`#3A77E6`)
+            .setTitle(`- THÀNH TÍCH -`)
+            .addField(`Tổ chức Member Event:`,`Is it your final choice? (25/3/2018)`)
+            .addField(`Giải nhất:`,`The Sky Challenges (Zapdos Challenge)`)
+            .addField(`Giải nhì:`,`The Sky Challenges (Articuno Challenge)`);
+
+            message.channel.sendEmbed(ME394);
+            message.channel.sendEmbed(ACME394);
+     }
 
     /// Pancham (ME969) ///
     if(args[0]==="ME969"| args[0]==="me969" | args[0]==="Me969" | args[0]==="Pancham" | args[0]==="pancham"){
