3b91bae40796439760815bcdb665e928bc6e7d94
Keiryto <shayrlslarkd56@gmail.com>
2018-05-19 23:06:51 +0700

    up

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ bot.login(settings.token);
 
 bot.on('ready', () => {
     console.log(`${bot.user.username} is ready!`);
-bot.user.setGame('10 days left.... (づ｡◕‿‿◕｡)づ')
+bot.user.setGame('10 days left (づ｡◕‿‿◕｡)づ')
 });
 
 bot.commands = new Discord.Collection();
