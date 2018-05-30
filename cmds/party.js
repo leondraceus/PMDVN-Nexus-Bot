c0e4458bbeb824c84b286578255a1daa1d47cfb1
leondraceus <hathaihiepluan@gmail.com>
2018-05-30 16:04:33 +0700

    up

--- cmds/party.js
+++ cmds/party.js
@@ -1,6 +1,16 @@
 module.exports.run = (pw, message, args) => {
-    message.channel.sendMessage(' ***LET`S PARTYYYYYYYYYY!!!!!!!!!!***\n(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ ✧ﾟ･: *ヽ(◕ヮ◕ヽ)\n┻━┻ ︵ヽ(`Д´)ﾉ︵ ┻━┻\n🎊🎊🎊🎉( ﾟヮﾟ)🎉🎊🎊🎊 ');
-    console.log(`${message.author.username}#${message.author.discriminator} party!!!`);
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    var roll = Math.floor(Math.random() * 3) +1;
+
+    if (roll == 1)
+    message.channel.sendMessage(' ***LET`S PARTYYYYYYYYYY!!!!!!!!!!***\n(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ ✧ﾟ･: *ヽ(◕ヮ◕ヽ) ');
+    else if (roll == 2)
+    message.channel.sendMessage(' ***LET`S PARTYYYYYYYYYY!!!!!!!!!!***\n┻━┻ ︵ヽ(`Д´)ﾉ︵ ┻━┻ ');
+    else if (roll == 3)
+    message.channel.sendMessage(' ***LET`S PARTYYYYYYYYYY!!!!!!!!!!***\n🎊🎊🎊🎉( ﾟヮﾟ)🎉🎊🎊🎊 ');
 }
 
 module.exports.help = {
