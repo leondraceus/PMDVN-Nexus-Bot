b3b8bf7ae8424441068014c1a652e090955e433f
leondraceus <hathaihiepluan@gmail.com>
2018-05-30 12:33:34 +0700

    up

--- cmds/vwheel.js
+++ cmds/vwheel.js
@@ -4,7 +4,7 @@ module.exports.run = async (pw, message, args) => {
     }
     var roll = Math.floor(Math.random() * 100) +1;
 
-    if (roll == 1 || roll == 2 || roll == 3 || roll == 4 || roll == 5 || roll == 6 || roll == 7 || roll == 8 || roll == 9 || roll == 10 || roll == 11 || roll == 12 || roll == 13 || roll == 14 || roll == 15 || roll == 16 || roll == 17 || roll == 18 || roll == 19 || roll == 20 || roll == 21 || roll == 22 || roll == 23 || roll == 24 || roll == 25)
+    if (roll == 1 || roll == 2 || roll == 3 || roll == 4 || roll == 5 || roll == 6 || roll == 7 || roll == 8 || roll == 9 || roll == 10 || roll == 11 || roll == 12 || roll == 13 || roll == 14 || roll == 15 || roll == 16 || roll == 17 || roll == 18 || roll == 19 || roll == 20 || roll == 21 || roll == 22 || roll == 23 || roll == 24 || roll == 25 || roll == 93 || roll == 97)
     message.reply("bạn đã quay trúng 10 <:poke:379535433467428867>, chúc mừng!");
 
     else if (roll == 21 || roll == 22 || roll == 23 || roll == 24 || roll == 25 || roll == 26 || roll == 27 || roll == 28 || roll == 29 || roll == 30 || roll == 31 || roll == 32 || roll == 33 || roll == 34 || roll == 35)
@@ -25,25 +25,25 @@ module.exports.run = async (pw, message, args) => {
     else if (roll == 68 || roll == 69 || roll == 70 || roll == 71 || roll == 72 || roll == 73 || roll == 74 || roll == 75 || roll == 76 || roll == 77)
     message.reply("bạn đã quay trúng Cream Puff, chúc mừng!");
 
-    else if (roll == 78 || roll == 79 || roll == 80 || roll == 81 || roll == 82)
+    else if (roll == 78 || roll == 79 || roll == 80 || roll == 81 || roll == 82 || roll == 94)
     message.reply("bạn đã quay trúng Coconut Water, chúc mừng!");
 
-    else if (roll == 83)
+    else if (roll == 83 || roll == 84 || roll == 99)
     message.reply("bạn đã quay trúng Cheesecake, chúc mừng!");
 
-    else if (roll == 84 || roll == 85 || roll == 86 || roll == 87 || roll == 88)
+    else if (roll == 85 || roll == 86 || roll == 87 || roll == 88 || roll == 89)
     message.reply("bạn đã quay trúng Heart Scale, chúc mừng!");
 
-    else if (roll == 89 || roll == 90 || roll == 91)
+    else if (roll == 90 || roll == 91)
     message.reply("bạn đã quay trúng Lucky Wheel Banner, chúc mừng!");
 
-    else if (roll == 92 || roll == 93 || roll == 94)
+    else if (roll == 92)
     message.reply("bạn đã quay trúng Victory Bell, chúc mừng!");
 
-    else if (roll == 95 || roll == 96 || roll == 97)
+    else if (roll == 95 || roll == 96)
     message.reply("bạn đã quay trúng Jukebox, chúc mừng!");
 
-    else if (roll == 98 || roll == 99)
+    else if (roll == 98)
     message.reply("bạn đã quay trúng Victini Doll, chúc mừng!");
 
     else if (roll == 100)
