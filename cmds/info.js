b8922b32d48ea828c082e133cea31609202e92a2
Keiryto <shayrlslarkd56@gmail.com>
2018-05-31 22:31:19 +0700

    up

--- cmds/info.js
+++ cmds/info.js
@@ -848,9 +848,9 @@ Vui lòng dùng code để tra cứu theo các lệnh sau: **.info ME459** | **.
             .addField(`Guild:`,`---`);
         var ACME072 = new Discord.RichEmbed().setColor(`#3A77E6`)
             .setTitle(`- THÀNH TÍCH -`)
-            .addField(`Tổ chức Member Event:`,`Pokémon Kicking Contest (24/12/2017 -> 1/1/2018)`)
-            .addField(`Tổ chức Member Event:`,`Time to Explode (1/1/2018 -> 6/1/2018)`)
-            .addField(`Tổ chức Member Event:`,`Fast and Furious (14/1/2018)`)
+            .addField(`Tổ chức Member Event:`,`Pokémon Kicking Contest (24/12/2017 -> 1/1/2018)
+            Time to Explode (1/1/2018 -> 6/1/2018)
+            Fast and Furious (14/1/2018)`)
             .addField(`Giải ba:`,`Heart Festival (Meowth Theater)`)
             .addField(`Tổ chức Member Event:`,`Berry Collector (25/2/2018 -> 25/2/2018)`)
             .addField(`Tổ chức Member Event:`,`Couple Race (18/3/2018)`)
