8b8227623fde80bf813e1db7285a6a67433c9cb3
Keiryto <shayrlslarkd56@gmail.com>
2018-06-06 13:42:33 +0700

    up

--- cmds/view.js
+++ cmds/view.js
@@ -28,7 +28,7 @@ module.exports.run = async (pw, message, args) => {
             .addField(`Gen 5 Starters & Electric Rodent Set`,`.view pack005`)
             .addField(`Gen 6 Starters & Electric Rodent Set`,`.view pack006`)
             .addField(`Gen 7 Starters & Electric Rodent Set`,`.view pack007`)
-            .addField(`Other`,`.view pack-other`);
+            .addField(`Other`,`.view packother`);
 
         message.channel.sendEmbed(doll);
     }
@@ -111,12 +111,12 @@ module.exports.run = async (pw, message, args) => {
         message.channel.sendEmbed(pack007);
     }
 
-    if(args[0]==="pack-other"){
-        var pack-other = new Discord.RichEmbed()
+    if(args[0]==="packother"){
+        var packother = new Discord.RichEmbed()
             .setTitle(`- Collectible Dolls -`).setColor(`#7FFFD4`).setURL(`http://pmdv-nexus.wikia.com/wiki/Kecleon's_Shop`).setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/5/59/Kecleon1.png/revision/latest/scale-to-width-down/56?cb=20170620115423`)
             .addField(`Eevee Doll (500 <:poke:379535433467428867>)`,`Một con búp bê hình Pokémon Eevee.`);
 
-        message.channel.sendEmbed(pack-other);
+        message.channel.sendEmbed(packother);
     }
 
     // Collectible Items //
