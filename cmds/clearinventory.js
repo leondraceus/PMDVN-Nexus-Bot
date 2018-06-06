f4fac4f376909b87b9944bf99d403f9e33073cf1
Keiryto <shayrlslarkd56@gmail.com>
2018-06-06 13:13:57 +0700

    up

--- cmds/clearinventory.js
+++ cmds/clearinventory.js
@@ -3,7 +3,7 @@
 module.exports.run = async (pw, message, args) => {
     var embed = new Discord.RichEmbed()
     //.setColor(`#556a74`)
-    .setDescription(`Inventory của`+ args[0] +` đã bị xóa sạch!`);
+    .setDescription(`Inventory của `+ args[0] +` đã bị xóa sạch!`);
 
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.sendMessage("```Tuổi lone gì mà đòi xóa ! Xóe !```");
     message.channel.sendEmbed(embed);
