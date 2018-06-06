2f8ca1be83cb9ec61da16c2add4bf14040b4c63a
Keiryto <shayrlslarkd56@gmail.com>
2018-06-06 13:27:18 +0700

    up

--- cmds/clearinventory.js
+++ cmds/clearinventory.js
@@ -6,6 +6,9 @@ module.exports.run = async (pw, message, args) => {
     .setDescription(`Inventory của `+ args[0] +` đã bị xóa sạch!`);
 
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.sendMessage("```Tuổi lone gì mà đòi xóa ! Xóe !```");
+
+    var toClearInv = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!toClearInv) return message.channel.sendMessage("```Hãy Mention nạn nhân của bạn!```");
     message.channel.sendEmbed(embed);
 }
 
