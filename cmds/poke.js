c111e971b160b7fcf6fd92cebb6f8ba7b68f6e2e
Keiryto <shayrlslarkd56@gmail.com>
2018-06-11 01:11:06 +0700

    up

--- cmds/poke.js
+++ cmds/poke.js
@@ -1,7 +1,7 @@
 module.exports.run = (pw, message, args) => {
     var toMention = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
     if(!toMention) return message.channel.sendMessage(`Bạn phải mention một ai đó!`)
-    message.channel.sendMessage(message.author.username + ' đã chọc ' + args[0] + ' 👉 ');
+    message.channel.sendMessage(message.author.username + ' đã chọt ' + args[0] + ' 👉 ');
 }
 
 module.exports.help = {
