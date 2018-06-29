bc8bb7f284d84e279de8c21bd114d669b50f5346
Keiryto <shayrlslarkd56@gmail.com>
2018-06-29 18:22:52 +0700

    up

---
+++ cmds/guildlist.js
@@ -0,0 +1,28 @@
+﻿const Discord = module.require("discord.js");
+
+module.exports.run = async (pw, message, args) => {
+    // NATURIA //
+    if(args[0]==="G002"){
+        var G002 = new Discord.RichEmbed()
+            .setTitle(`- NATURIA -`).setColor(`#7FFFD4`).setURL(`https://pmdv-nexus.wikia.com/wiki/Naturia`).setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/f/f5/Guild_Leader_Badge.png/revision/latest?cb=20180616005707`)
+            .addField(`Rank`,`Starter`)
+            .addField(`Ngày thành lập`,`20/6/2018`)
+            .addField(`Hội trưởng`,`Torterra (ME563)`)
+            .addField(`Guild Base Theme`,`N/A`)
+            .addField(`Số lượng thành viên tối đa`,`5`);
+        var ACG002 = new Discord.RichEmbed().setColor(`#7FFFD4`)
+            .setTitle(`- THÀNH VIÊN CỦA GUILD -`).setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/0/05/Torterra.png/revision/latest/scale-to-width-down/80?cb=20171128145511`)
+            .addField(`Torterra (ME563)`,`Cấp bậc: Hội trưởng`)
+            .addField(`Serperior (ME868)`,`Cấp bậc: Thành viên`)
+            .addField(`Sceptile (ME855)`,`Cấp bậc: Thành viên`)
+            .addField(`Lilligant (ME571)`,`Cấp bậc: Thành viên`)
+            .addField(`Decidueye (ME330)`,`Cấp bậc: Thành viên`);
+
+        message.channel.sendEmbed(G002);
+        message.channel.sendEmbed(ACG002);
+    }
+}
+
+module.exports.help = {
+    name: "guild"
+}
