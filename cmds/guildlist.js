13fc3e14a27fb9859bd034d81160bb187fdf7bcf
Keiryto <shayrlslarkd56@gmail.com>
2018-06-29 18:27:27 +0700

    up

--- cmds/guildlist.js
+++ cmds/guildlist.js
@@ -2,15 +2,15 @@
 
 module.exports.run = async (pw, message, args) => {
     // NATURIA //
-    if(args[0]==="G002"){
+    if(args[0]==="G002"| args[0]==="g002"){
         var G002 = new Discord.RichEmbed()
-            .setTitle(`- NATURIA -`).setColor(`#7FFFD4`).setURL(`https://pmdv-nexus.wikia.com/wiki/Naturia`).setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/f/f5/Guild_Leader_Badge.png/revision/latest?cb=20180616005707`)
+            .setTitle(`- NATURIA -`).setColor(`#FF69B4`).setURL(`https://pmdv-nexus.wikia.com/wiki/Naturia`).setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/f/f5/Guild_Leader_Badge.png/revision/latest?cb=20180616005707`)
             .addField(`Rank`,`Starter`)
             .addField(`Ngày thành lập`,`20/6/2018`)
             .addField(`Hội trưởng`,`Torterra (ME563)`)
             .addField(`Guild Base Theme`,`N/A`)
             .addField(`Số lượng thành viên tối đa`,`5`);
-        var ACG002 = new Discord.RichEmbed().setColor(`#7FFFD4`)
+        var ACG002 = new Discord.RichEmbed().setColor(`#FF69B4`)
             .setTitle(`- THÀNH VIÊN CỦA GUILD -`).setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/0/05/Torterra.png/revision/latest/scale-to-width-down/80?cb=20171128145511`)
             .addField(`Torterra (ME563)`,`Cấp bậc: Hội trưởng`)
             .addField(`Serperior (ME868)`,`Cấp bậc: Thành viên`)
