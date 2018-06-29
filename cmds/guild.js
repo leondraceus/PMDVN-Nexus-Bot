115bbee50713773e20a9ba22fa8ea9e5191bcd42
Keiryto <shayrlslarkd56@gmail.com>
2018-06-29 17:53:10 +0700

    up

--- cmds/guild.js
+++ cmds/guild.js
@@ -2,7 +2,7 @@
 
 module.exports.run = async (pw, message, args) => {
     var embed = new Discord.RichEmbed().setColor(`#FF69B4`)
-        .setTitle(`Guild Club`)
+        .setTitle(`- GUILD CLUB -`).setURL(`https://pmdv-nexus.wikia.com/wiki/Guild_Club`)
         .setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/f/f1/Wigglytuff.png/revision/latest/scale-to-width-down/56?cb=20180303141617`)
         .setDescription(`Chào mừng đến với Câu lạc bộ Hội nhóm (Guild Club). Đây là nơi để bạn có thể thành lập 1 Guild cho riêng mình và cùng với những Pokémon khác xây dựng nó.`)
         .addField(`E-Gang`,`.guild G001`)
