93727fbb3cbebeb4d3388c8d363b7c156cfc425e
Keiryto <shayrlslarkd56@gmail.com>
2018-06-29 20:19:56 +0700

    up

--- cmds/guildlist.js
+++ cmds/guildlist.js
@@ -1,6 +1,28 @@
 ﻿const Discord = module.require("discord.js");
 
 module.exports.run = async (pw, message, args) => {
+
+    // E-Gang //
+    if(args[0]==="G001"| args[0]==="g001"){
+        var G001 = new Discord.RichEmbed()
+            .setTitle(`- E-GANG -`).setColor(`#FF69B4`).setURL(`https://pmdv-nexus.wikia.com/wiki/E-Gang`).setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/f/f5/Guild_Leader_Badge.png/revision/latest?cb=20180616005707`)
+            .addField(`Rank`,`Starter`)
+            .addField(`Ngày thành lập`,`15/6/2018`)
+            .addField(`Hội trưởng`,`Espeon (ME906)`)
+            .addField(`Guild Base Theme`,`N/A`)
+            .addField(`Số lượng thành viên tối đa`,`5`);
+        var ACG001 = new Discord.RichEmbed().setColor(`#FF69B4`)
+            .setTitle(`- THÀNH VIÊN CỦA GUILD -`).setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/e/ef/Espeon.png/revision/latest/scale-to-width-down/80?cb=20160918160608`)
+            .addField(`Espeon (ME906)`,`Cấp bậc: Hội trưởng`)
+            .addField(`Jolteon (ME684)`,`Cấp bậc: Thành viên`)
+            .addField(`Flareon (ME008)`,`Cấp bậc: Thành viên`)
+            .addField(`Vaporeon (ME014)`,`Cấp bậc: Thành viên`)
+            .addField(`Jirachi (ME394)`,`Cấp bậc: Thành viên`);
+
+        message.channel.sendEmbed(G001);
+        message.channel.sendEmbed(ACG001);
+    }
+
     // NATURIA //
     if(args[0]==="G002"| args[0]==="g002"){
         var G002 = new Discord.RichEmbed()
