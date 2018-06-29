a3fd84d5a42d62ddbe1fbb2abbf31f6efb5d1e71
Keiryto <shayrlslarkd56@gmail.com>
2018-06-29 20:30:15 +0700

    up

--- cmds/guildlist.js
+++ cmds/guildlist.js
@@ -10,7 +10,8 @@ module.exports.run = async (pw, message, args) => {
             .addField(`Ngày thành lập`,`15/6/2018`)
             .addField(`Hội trưởng`,`Espeon (ME906)`)
             .addField(`Guild Base Theme`,`N/A`)
-            .addField(`Số lượng thành viên tối đa`,`5`);
+            .addField(`Số lượng thành viên tối đa`,`5`)
+            .setFooter(`Guild được thành lập dành cho những bạn yêu thích và đã là một trong các Eeveelutions.);
         var ACG001 = new Discord.RichEmbed().setColor(`#FF69B4`)
             .setTitle(`- THÀNH VIÊN CỦA GUILD -`).setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/e/ef/Espeon.png/revision/latest/scale-to-width-down/80?cb=20160918160608`)
             .addField(`Espeon (ME906)`,`Cấp bậc: Hội trưởng`)
@@ -31,8 +32,7 @@ module.exports.run = async (pw, message, args) => {
             .addField(`Ngày thành lập`,`20/6/2018`)
             .addField(`Hội trưởng`,`Torterra (ME563)`)
             .addField(`Guild Base Theme`,`N/A`)
-            .addField(`Số lượng thành viên tối đa`,`5`)
-            .addField(`Giới thiệu về Guild:`,`Guild được thành lập dành cho những bạn yêu thích và đã là một trong các Eeveelutions.`);
+            .addField(`Số lượng thành viên tối đa`,`5`);
         var ACG002 = new Discord.RichEmbed().setColor(`#FF69B4`)
             .setTitle(`- THÀNH VIÊN CỦA GUILD -`).setThumbnail(`https://vignette.wikia.nocookie.net/pmdv-nexus/images/0/05/Torterra.png/revision/latest/scale-to-width-down/80?cb=20171128145511`)
             .addField(`Torterra (ME563)`,`Cấp bậc: Hội trưởng`)
