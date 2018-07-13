69779b29d62d29fd78c8660f6771f2c1b6eca4bf
Keiryto <shayrlslarkd56@gmail.com>
2018-07-13 22:27:27 +0700

    up

--- cmds/treasurebox.js
+++ cmds/treasurebox.js
@@ -2,35 +2,47 @@
     function getRandomInt(min, max) {
        return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-var roll = Math.floor(Math.random() * 100) +1;
+var roll = Math.floor(Math.random() * 60) +1;
 
-if (roll == 5 || roll == 10 || roll == 15 || roll == 20 || roll == 25 || roll == 30 || roll == 35 || roll == 40 || roll == 45 || roll == 50 || roll == 55 || roll == 60 || roll == 65 || roll == 70 || roll == 75 || roll == 80 || roll == 85 || roll == 90 || roll == 95 || roll == 100 || roll == 4 || roll == 6 || roll == 14 || roll == 19 || roll == 24)
-    message.channel.sendMessage("Bạn đã nhận được 100 <:poke:379535433467428867> từ hòm kho báu ! Chúc mừng");
+if (roll == 1 || roll == 13 || roll == 15 || roll == 36 || roll == 59)
+    message.channel.sendMessage("Số 1");
 
-else if (roll == 4 || roll == 8 || roll == 12|| roll == 16 || roll == 28|| roll == 32 || roll == 36 || roll == 44 || roll == 48 || roll == 52 || roll == 56 || roll == 64 || roll == 68 || roll == 72 || roll == 76 || roll == 84 || roll == 88 || roll == 92 || roll == 96 || roll == 2)
-    message.channel.sendMessage("Bạn đã nhận được 150 <:poke:379535433467428867> từ hòm kho báu ! Chúc mừng");
+else if (roll == 2 || roll == 14 || roll == 35|| roll == 37 || roll == 58)
+    message.channel.sendMessage("Số 2");
 
-else if (roll == 1 || roll == 3 || roll == 7|| roll == 9 || roll == 11|| roll == 13 || roll == 17|| roll == 21 || roll == 23 || roll == 27 || roll == 29 || roll == 31 || roll == 33 || roll == 37 || roll == 39)
-    message.channel.sendMessage("Bạn đã nhận được 200 <:poke:379535433467428867> từ hòm kho báu ! Chúc mừng");
+else if (roll == 3 || roll == 15 || roll == 34|| roll == 38 || roll == 57)
+    message.channel.sendMessage("Số 3");
 
-else if (roll == 41 || roll == 43 || roll == 47 || roll == 49 || roll == 51 || roll == 53 || roll == 57|| roll == 59 || roll == 61 || roll == 63 || roll == 67 || roll == 69 || roll == 71 || roll == 73 || roll == 77)
-    message.channel.sendMessage("Bạn đã nhận được 250 <:poke:379535433467428867> từ hòm kho báu ! Chúc mừng");
+else if (roll == 4 || roll == 16 || roll == 33 || roll == 39 || roll == 56)
+    message.channel.sendMessage("Số 4");
 
-else if (roll == 79 || roll == 81 || roll == 83|| roll == 87 || roll == 89|| roll == 91 || roll == 93|| roll == 97 || roll == 99|| roll == 18)
-    message.channel.sendMessage("Bạn đã nhận được 300 <:poke:379535433467428867> từ hòm kho báu ! Chúc mừng");
+else if (roll == 5 || roll == 17 || roll == 32|| roll == 40 || roll == 55)
+    message.channel.sendMessage("Số 5");
 
-else if (roll == 22 || roll == 26 || roll == 34|| roll == 38 || roll == 54|| roll == 42 || roll == 46)
-    message.channel.sendMessage("Bạn đã nhận được 325 <:poke:379535433467428867> từ hòm kho báu ! Chúc mừng");
+else if (roll == 6 || roll == 18 || roll == 31|| roll == 41 || roll == 54)
+    message.channel.sendMessage("Số 6");
 
-else if (roll == 54 || roll == 58 || roll == 62|| roll == 66 || roll == 74)
-    message.channel.sendMessage("Bạn đã nhận được 350 <:poke:379535433467428867> từ hòm kho báu ! Chúc mừng");
+else if (roll == 7 || roll == 19 || roll == 30|| roll == 42 || roll == 53)
+    message.channel.sendMessage("Số 7");
 
-else if (roll == 78 || roll == 82 || roll == 86)
-    message.channel.sendMessage("Bạn đã nhận được 400 <:poke:379535433467428867> từ hòm kho báu ! Chúc mừng");
+else if (roll == 8 || roll == 20 || roll == 29 || roll == 43 || roll == 52)
+    message.channel.sendMessage("Số 8");
+
+else if (roll == 9 || roll == 21 || roll == 28|| roll == 44 || roll == 51)
+    message.channel.sendMessage("Số 9");
+
+else if (roll == 10 || roll == 22 || roll == 27|| roll == 45 || roll == 50)
+    message.channel.sendMessage("Số 10");
+
+else if (roll == 11 || roll == 23 || roll == 26|| roll == 46 || roll == 49)
+    message.channel.sendMessage("Số 11");
+
+else if (roll == 12 || roll == 24 || roll == 25|| roll == 47 || roll == 48)
+    message.channel.sendMessage("Số 12");
 
 }
 
 module.exports.help = {
-    name: "treasurebox"
+    name: "tor"
 }
 
