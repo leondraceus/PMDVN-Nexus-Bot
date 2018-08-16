283a0f7a08b2db1230de311dba7e5fb5769c82bb
ThunderDraconius <34503062+ThunderDraconius@users.noreply.github.com>
2018-08-16 15:22:13 +0700

    up

--- cmds/slap.js
+++ cmds/slap.js
@@ -1,7 +1,7 @@
 module.exports.run = (pw, message, args) => {
     var toMention = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
     if(!toMention) return message.channel.sendMessage(`Bạn phải mention một ai đó!`)
-    message.channel.sendMessage(message.author.username + ' đã tát sml ' + args[0] + '! https://www.technobuffalo.com/wp-content/uploads/2016/07/Chansy-slap.gif');
+    message.channel.sendMessage(message.author.username + ' đã tát sml ' + args[0] + '! http://gifsstore.com/public/upload/gifs/15343399831534339977.gif');
     console.log(`${message.author.username}#${message.author.discriminator} used slap!`);
 }
 
