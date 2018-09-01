f513e66a1e61b53a1e77567444cac15bc980799a
shayrl56 <shayrlslarkd56@gmail.com>
2018-09-01 11:27:16 +0700

    up

--- cmds/magicwand.js
+++ cmds/magicwand.js
@@ -1,9 +1,13 @@
 ﻿// JavaScript source code
 module.exports.run = async (pw, message, args) => {
-    var roll = Math.floor(Math.random() * 10) +1;
+    var roll = Math.floor(Math.random() * 5) +1;
 
     if (roll == 1)
         message.reply("bạn đã nhận được 1 Spooky Token!");
+
+    else if (roll == 0)
+        message.reply("Tiếc quá ! Bạn không nhận được gì cả !");
+
     else if (roll != 1)
     message.reply("bạn đã nhận được " + roll + " Spooky Tokens!");
 }
