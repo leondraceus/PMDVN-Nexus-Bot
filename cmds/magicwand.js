2615ac5223c218e28cdb0e0eda960f1c54bab01a
ThunderDraconius <34503062+ThunderDraconius@users.noreply.github.com>
2018-09-03 08:37:54 +0700

    up

--- cmds/magicwand.js
+++ cmds/magicwand.js
@@ -3,13 +3,13 @@ module.exports.run = async (pw, message, args) => {
     var roll = Math.floor(Math.random() * 5) +1;
 
     if (roll == 1)
-        message.reply("bạn đã nhận được 1 Spooky Token!");
+        message.reply("bạn đã nhận được 1 Spooky Token! :magicalwand:");
 
     else if (roll == 0)
         message.reply("Tiếc quá ! Bạn không nhận được gì cả !");
 
     else 
-    message.reply("bạn đã nhận được " + roll + " Spooky Tokens!");
+    message.reply("bạn đã nhận được " + roll + " Spooky Tokens! :magicalwand:");
 }
 
 module.exports.help = {
