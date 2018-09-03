a922abbbf9c5f671200ad8cd0cfb473af65cad18
ThunderDraconius <34503062+ThunderDraconius@users.noreply.github.com>
2018-09-03 10:33:49 +0700

    up

--- cmds/grabbag.js
+++ cmds/grabbag.js
@@ -3,12 +3,12 @@ module.exports.run = async (pw, message, args) => {
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    var roll = Math.floor(Math.random() * 100) +1;
+    var roll = Math.floor(Math.random() * 150) +1;
 
-    if (roll == 29 || roll == 11 || roll == 19 || roll == 90 || roll == 25 || roll == 6 || roll == 27 || roll == 60 || roll == 9 || roll == 98)
+    if (roll == 29 || roll == 11 || roll == 19 || roll == 90 || roll == 25 || roll == 6 || roll == 27 || roll == 60 || roll == 9 || roll == 98 || roll == 86 || roll == 4 || roll == 58 || roll == 72 || roll == 85 || roll == 23 || roll == 95 || roll == 96 || roll == 71 || roll == 111 || roll == 112 || roll == 113 || roll == 114 || roll == 115 || roll == 116 || roll == 117 || roll == 118 || roll == 119 || roll == 120 || roll == 121 || roll == 122 || roll == 123 || roll == 124 || roll == 125 || roll == 126 || roll == 127 || roll == 128 || roll == 129 || roll == 130)
         message.reply("trong Grab Bag có 1 Stardust, chúc mừng!");
 
-    else if (roll == 2 || roll == 12 || roll == 48 || roll == 14 || roll == 97 || roll == 55 || roll == 17 || roll == 83)
+    else if (roll == 2 || roll == 12 || roll == 48 || roll == 14 || roll == 97 || roll == 55 || roll == 17 || roll == 83 || roll == 26 || roll == 35)
         message.reply("trong Grab Bag có 3 Stardust, chúc mừng!");
 
     else if (roll == 3 || roll == 20 || roll == 81 || roll == 22)
@@ -17,7 +17,7 @@ module.exports.run = async (pw, message, args) => {
     else if (roll == 87 || roll == 24)
         message.reply("trong Grab Bag có 10 Stardust, chúc mừng!");
 
-    else if (roll == 5 || roll == 26)
+    else if (roll == 5)
         message.reply("trong Grab Bag có Friend Bow, chúc mừng!");
 
     else if (roll == 93 || roll == 28)
@@ -25,6 +25,9 @@ module.exports.run = async (pw, message, args) => {
 
     else if (roll == 1)
         message.reply("trong Grab Bag có Miracle Chest, chúc mừng!");
+
+    else if (roll == 101 || roll == 102 || roll == 103 || roll == 104 || roll == 105 || roll == 106 || roll == 107 || roll == 108 || roll == 109 || roll == 110 || roll == 131 || roll == 132 || roll == 133 || roll == 134 || roll == 135 || roll == 136 || roll == 137 || roll == 138 || roll == 139 || roll == 140 || roll == 141 || roll == 142 || roll == 143 || roll == 144 || roll == 145 || roll == 146 || roll == 147 || roll == 148 || roll == 149 || roll == 150)
+        message.reply("trong Grab Bag có 1 Wooden Stick, chúc mừng!");    
         
     else if (roll == 82 || roll == 31 || roll == 78 || roll == 33 || roll == 34 || roll == 94 || roll == 36 || roll == 76 || roll == 38 || roll == 39)
         message.reply("trong Grab Bag có 3 Wooden Stick, chúc mừng!");
@@ -56,13 +59,13 @@ module.exports.run = async (pw, message, args) => {
     else if (roll == 30)
         message.reply("trong Grab Bag có 1 Prism, chúc mừng!");
 
-    else if (roll == 18 || roll == 54 || roll == 85 || roll == 86 || roll == 4)
+    else if (roll == 18 || roll == 54)
         message.reply("trong Grab Bag có 1 Sun Ribbon, chúc mừng!");
 
-    else if (roll == 53 || roll == 89 || roll == 23 || roll == 58 || roll == 72)
+    else if (roll == 53 || roll == 89)
         message.reply("trong Grab Bag có 1 Lunar Ribbon, chúc mừng!");
 
-    else if (roll == 45 || roll == 35 || roll == 95 || roll == 96 || roll == 71)
+    else if (roll == 45)
         message.reply("trong Grab Bag có 1 Oval Stone, chúc mừng!");
 
     else if (roll == 10)
