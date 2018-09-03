7fbaeaa7e5e9c5667064dd04d9de5b9aca11a051
ThunderDraconius <34503062+ThunderDraconius@users.noreply.github.com>
2018-09-03 11:09:04 +0700

    up

--- cmds/help.js
+++ cmds/help.js
@@ -5,16 +5,10 @@ module.exports.run = async (pw, message, args) => {
         .setThumbnail(`https://vignette4.wikia.nocookie.net/pmdv-nexus/images/8/89/Wiki-wordmark.png/revision/latest?cb=20170613014107`)
         .setTitle(`**__PMDVN-NEXUS__**`)
         .setDescription(`__Pokémon Mystery Dungeon Vietnam Bot__`)
-        .addField(`1. THÔNG TIN CÁ NHÂN`,`---------------`)
-        .addField(`.info <member code/pokemon>`,`Xem thông tin cơ bản`)
-        .addField(`.inv <member code/pokemon>`,`Xem tiền và Items trong vỏ đồ`)
-        .addField(`.skill <member code/pokemon>`,`Xem Pokémon Skill`)
-        .addField(`2. TIỆN ÍCH`,`---------------`)
         .addField(`.shop`,`Xem các mặt hàng trên Kecleon's Shop.`)
         .addField(`.guildclub`,`Xem thông tin về các guild.`)
-        .addField(`3. GIẢI TRÍ`,`---------------`)
-        .addField(`\u200B`,`.bite | .slap | .smash | .poke | .hug | 
-.kiss | .lick | .intensify | .trigger | .explode`,inline = true)
+        .addField(`.bite | .slap | .smash | .poke | .hug | 
+.kiss | .lick | .intensify | .trigger | .explode`,`Các cmd giải trí.`)
         .setColor(`#a366ff`);
    
     message.channel.sendEmbed(embed);
