7bf4d3d5ed96ec34e62191635d099077578838cc
ThunderDraconius <34503062+ThunderDraconius@users.noreply.github.com>
2018-10-27 19:49:29 +0700

    up

--- cmds/vwheel.js
+++ cmds/vwheel.js
@@ -4,14 +4,17 @@ module.exports.run = async (pw, message, args) => {
     }
     var roll = Math.floor(Math.random() * 100) +1;
 
-    if (roll == 3 || roll == 96 || roll == 5 || roll == 6 || roll == 92 || roll == 8 || roll == 9 || roll == 66 || roll == 69 || roll == 12 || roll == 13 || roll == 14 || roll == 15 || roll == 16 || roll == 17 || roll == 18 || roll == 19 || roll == 20 || roll == 21 || roll == 22)
+    if (roll == 3 || roll == 96 || roll == 5 || roll == 6 || roll == 92 || roll == 8 || roll == 9 || roll == 66 || roll == 69 || roll == 12 || roll == 13 || roll == 14 || roll == 15 || roll == 16 || roll == 17 || roll == 18 || roll == 19 || roll == 20 || roll == 21 || roll == 22 || roll == 90 || roll == 91 || roll == 7)
     message.reply("bạn đã quay trúng 50 <:poke:379535433467428867>, chúc mừng!");
 
-    else if (roll == 1 || roll == 99 || roll == 64 || roll == 24 || roll == 25 || roll == 26 || roll == 71 || roll == 65 || roll == 29 || roll == 30 || roll == 31 || roll == 32 || roll == 33 || roll == 34 || roll == 35)
+    else if (roll == 1 || roll == 99 || roll == 64 || roll == 24 || roll == 25 || roll == 26 || roll == 71 || roll == 65 || roll == 29 || roll == 30 || roll == 31 || roll == 32 || roll == 33 || roll == 34 || roll == 35 || roll == 84 || roll == 85 || roll == 86 || roll == 87 || roll == 88 || roll == 89)
     message.reply("bạn đã quay trúng 100 <:poke:379535433467428867>, chúc mừng!");
 
     else if (roll == 68 || roll == 37 || roll == 38 || roll == 39 || roll == 40 || roll == 98 || roll == 42 || roll == 43 || roll == 44 || roll == 45)
     message.reply("bạn đã quay trúng 200 <:poke:379535433467428867>, chúc mừng!");
+
+    else if (roll == 74 || roll == 75 || roll == 76 || roll == 77 || roll == 78 || roll == 79 || roll == 80 || roll == 81 || roll == 82 || roll == 83)
+    message.reply("bạn đã quay trúng 300 <:poke:379535433467428867>, chúc mừng!");
 
     else if (roll == 46 || roll == 47 || roll == 48 || roll == 49 || roll == 50)
     message.reply("bạn đã quay trúng 500 <:poke:379535433467428867>, chúc mừng!");
@@ -30,15 +33,6 @@ module.exports.run = async (pw, message, args) => {
 
     else if (roll == 27 || roll == 72 || roll == 73)
     message.reply("bạn đã quay trúng Fire Stone / Thunder Stone / Water Stone / Sun Ribbon / Lunar Ribbon / Mossy Rock / Frozen Rock / Affection Heart, chúc mừng!\n```Chọn 1 trong số món đồ trên.```");
-        
-    else if (roll == 74 || roll == 75 || roll == 76 || roll == 77 || roll == 78 || roll == 79 || roll == 80 || roll == 81 || roll == 82 || roll == 83)
-    message.reply("bạn đã quay trúng Cotton Candy, chúc mừng!");
-
-    else if (roll == 84 || roll == 85 || roll == 86 || roll == 87 || roll == 88 || roll == 89)
-    message.reply("bạn đã quay trúng Watermelon Juice, chúc mừng!");
-
-    else if (roll == 90 || roll == 91 || roll == 7)
-    message.reply("bạn đã quay trúng Tiramisu, chúc mừng!");
 
     else if (roll == 93 || roll == 94)
     message.reply("bạn đã quay trúng Lucky Wheel Banner, chúc mừng!");
