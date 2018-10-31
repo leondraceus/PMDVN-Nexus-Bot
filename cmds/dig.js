31a543cb89a10f3c8c43041e33599236cec77747
ThunderDraconius <34503062+ThunderDraconius@users.noreply.github.com>
2018-10-31 18:41:53 +0700

    up

--- cmds/dig.js
+++ cmds/dig.js
@@ -2,12 +2,14 @@ module.exports.run = async (pw, message, args) => {
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    var treasure = Math.floor(Math.random() * 20) +1;
+    var treasure = Math.floor(Math.random() * 50) +1;
 
     if (treasure < 10)
     message.reply("bạn không đào được gì cả.");
-    else 
+    else (treasure == 11 || treasure == 13)
     message.reply("bạn đào được 1 Gold Bar.");
+    else
+    message.reply("bạn không đào được gì cả.");
 
 }
 module.exports.help = {
